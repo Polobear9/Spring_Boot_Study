915291437f5c09adbe0bac8bcd7cebbd6069d42c
Polobear9 <lieams1996@gmail.com>
2022-09-16 09:43:02 +0900

    add test unit. use a Junit5

---
+++ src/test/java/repository/Memory_Member_RepositoryTest.java
@@ -0,0 +1,24 @@
+package repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Memory_Member_RepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}

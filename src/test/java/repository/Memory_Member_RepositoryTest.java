6b49b9978b2962ecdacf5fe93fe6b1bf84bb3ccc
Polobear9 <lieams1996@gmail.com>
2022-09-16 09:59:10 +0900

    make a test case, use a junit5. test-case --> memberservice.save(); this pass.

--- src/test/java/repository/Memory_Member_RepositoryTest.java
+++ src/test/java/repository/Memory_Member_RepositoryTest.java
@@ -1,13 +1,35 @@
 package repository;
 
+import Service.MemberService;
+import domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Memory_Member_RepositoryTest {
 
+
+    MemberService memberService = new MemberService();
+
+
     @Test
-    void save() {
+    void save() { //save --> join the repository
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+
+        //when
+        Long saveId = memberService.join(member);
+
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+
     }
 
     @Test

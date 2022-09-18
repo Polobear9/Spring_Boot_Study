0b68df0ca9cf644063f73c8628ad0050cacf4060
Polobear9 <lieams1996@gmail.com>
2022-09-18 21:33:56 +0900

    create the member create html and controller.

--- src/main/java/com/example/demo/Controller/MemberController.java
+++ src/main/java/com/example/demo/Controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller;
 import com.example.demo.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -11,6 +12,11 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 
 

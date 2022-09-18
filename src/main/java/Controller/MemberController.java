eb6d975fee1afe97a12538d6e28e3d450375f600
Polobear9 <lieams1996@gmail.com>
2022-09-18 17:46:35 +0900

    user a membercontroller --> use a spring container. @Autowired

--- src/main/java/Controller/MemberController.java
+++ src/main/java/Controller/MemberController.java
@@ -1,10 +1,17 @@
 package Controller;
 
 import Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService = new MemberService();
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
 
 }

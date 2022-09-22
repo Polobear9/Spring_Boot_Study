1a582e87ee79e958a258ef2b8d77dc4ea3f50d0e
Polobear9 <lieams1996@gmail.com>
2022-09-22 20:27:28 +0900

    Add the Jdbc connection data.

--- src/main/java/com/example/demo/SpringConfig.java
+++ src/main/java/com/example/demo/SpringConfig.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
 import com.example.demo.Service.MemberService;
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.Memory_Member_Repository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public Memory_Member_Repository memory_member_repository() {
-        return new Memory_Member_Repository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/com/example/demo/repository/JdbcMemberRepository.java
+++ src/main/java/com/example/demo/repository/JdbcMemberRepository.java
@@ -1,22 +1,23 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
-public class JdbcMemberRepository implements Member_Repository{
+import static org.apache.tomcat.util.descriptor.InputSourceUtil.close;
+
+public class JdbcMemberRepository implements Member_Repository {
 
     private final DataSource dataSource;
 
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)"; //param binding
@@ -25,13 +26,24 @@ public class JdbcMemberRepository implements Member_Repository{
         ResultSet rs = null;
 
         try {
-            conn = pstmt.getConnection();
+            conn = getConnection();
             pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             pstmt.setString(1, member.getName());
 
             pstmt.executeUpdate();
             rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("can not find member ID");
+            }
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
         }
         return null;
     }
@@ -54,5 +66,33 @@ public class JdbcMemberRepository implements Member_Repository{
     @Override
     public List<Member> findAll() {
         return null;
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }

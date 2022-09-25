e115874573446fd2a89f4ed059aa6cb8d78879c7
Polobear9 <lieams1996@gmail.com>
2022-09-25 14:31:31 +0900

    Add the Spring base Test code.

---
+++ src/test/java/Service/MemberServiceIntegrationTest.java
@@ -0,0 +1,51 @@
+package Service;
+
+import com.example.demo.Service.MemberService;
+import com.example.demo.domain.Member;
+import com.example.demo.repository.Memory_Member_Repository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired Memory_Member_Repository memory_member_repository;
+
+    @Test
+    void join() {
+        //give
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void sameMemberFind() {
+        //give
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
+}

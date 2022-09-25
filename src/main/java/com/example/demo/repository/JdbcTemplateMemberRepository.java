8dffaf79a38f13d38ba40b3a9bd5e595f5bb6215
Polobear9 <lieams1996@gmail.com>
2022-09-25 14:54:18 +0900

    Add save Jdbc code.

--- src/main/java/com/example/demo/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/com/example/demo/repository/JdbcTemplateMemberRepository.java
@@ -4,11 +4,15 @@ import com.example.demo.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class JdbcTemplateMemberRepository implements Member_Repository{
@@ -22,7 +26,15 @@ public class JdbcTemplateMemberRepository implements Member_Repository{
 
     @Override
     public Member save(Member member) {
-        return null;
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
     }
 
     @Override
